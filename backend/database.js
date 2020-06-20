0e2e0ec72469eac5278d7695371beddd6bbb1066
Alex Aranda <alexarand32@gmail.com>
2020-06-20 13:42:09 -0500

    comit final

--- backend/database.js
+++ backend/database.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose');
 
 //Connect DB
-<<<<<<< HEAD
 const URI = 'mongodb://localhost/alimentos'; //Se crea BD
 
 mongoose.connect(URI, { useNewUrlParser: true })
-=======
-//const URI = 'mongodb+srv://cj7:<cj7>@app-food-7qqkl.mongodb.net/test'; //Se crea BD
-//Base de datos en la nube
-const URI = 'mongodb+srv://cj7:cj7@app-food-7qqkl.mongodb.net/alimentos?retryWrites=true&w=majority';
-mongoose.connect(URI)
->>>>>>> 385dbdc2bead4140c4d2187d5aa820c6682143a3
     .then(db => console.log('DB is connected'))
     .catch(err=> console.error(err));
 
