a3071e9df5927514f076b9a8010c7b18019b4279
Accel <axel_manuel@outlook.es>
2020-07-01 14:35:05 -0500

    corrigiendo errores de la fusion con la rama feature

--- backend/database.js
+++ backend/database.js
@@ -2,15 +2,10 @@ const mongoose = require('mongoose');
 
 //Connect DB
 const URI = 'mongodb://localhost/alimentos'; //Se crea BD
-<<<<<<< HEAD
-//cambios para la rama desarrollo
-mongoose.connect(URI, { useNewUrlParser: true })
-=======
 //Base de datos en la nube   
 
 //const URI = 'mongodb+srv://cj7:cj7@app-food-7qqkl.mongodb.net/alimentos?retryWrites=true&w=majority';
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
->>>>>>> new-feature
     .then(db => console.log('DB is connected'))
     .catch(err=> console.error(err));
 
