f1da11095409457c1db89b3a36ad6e8c85ad21a7
Accel <axel_manuel@outlook.es>
2020-07-30 10:08:26 -0500

    Fusion con lo de chivo

--- frontend/src/app/models/categoria-alimento.spec.ts
+++
@@ -1,7 +0,0 @@
-import { CategoriaAlimento } from './categoria-alimento';
-
-describe('CategoriaAlimento', () => {
-  it('should create an instance', () => {
-    expect(new CategoriaAlimento()).toBeTruthy();
-  });
-});
--- frontend/src/app/models/subcategoria-alimento.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SubcategoriaAlimento } from './subcategoria-alimento';
-
-describe('SubcategoriaAlimento', () => {
-  it('should create an instance', () => {
-    expect(new SubcategoriaAlimento()).toBeTruthy();
-  });
-});
